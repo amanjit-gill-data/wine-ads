e73428fc4bc45bf7d8d5ddce5eb3e882ecd1f1f7
Amanjit Gill <amanjitgilldata@gmail.com>
2024-10-03 03:25:33 +1000

    minimal flask app

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/amanjit")
-def amanjit():
-    return "this is amanjit's page"
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
