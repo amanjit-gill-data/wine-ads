3a94e871670ab715e14921b5365ccea11f833211
Amanjit Gill <amanjitgilldata@gmail.com>
2024-10-02 15:31:49 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+# %% 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/amanjit")
+def amanjit():
+    return "this is amanjit's page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# %% 
+
+
